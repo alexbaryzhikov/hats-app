f96e615c001b4ccf1f2546c7c57b8e5322afae17
Alex Baryzhikov <aleksiarts@gmail.com>
2019-02-09 22:12:31 +0300

    Sending images (WIP)

--- app/src/main/java/com/alexbaryzhikov/hatsapp/activities/ChatActivity.kt
+++ app/src/main/java/com/alexbaryzhikov/hatsapp/activities/ChatActivity.kt
@@ -1,5 +1,7 @@
 package com.alexbaryzhikov.hatsapp.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -18,15 +20,18 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.item_image.view.*
 import kotlinx.android.synthetic.main.item_message.view.*
 
 private const val TAG = "ChatActivity"
+private const val GET_IMAGES_CODE = 1
 
 class ChatActivity : AppCompatActivity() {
 
     private lateinit var chatId: String
     private lateinit var chatDb: DatabaseReference
     private val messageAdapter = MessageAdapter()
+    private val imageUriAdapter = ImageUriAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,9 +45,26 @@ class ChatActivity : AppCompatActivity() {
         chatDb = db.reference.child("chat").child(chatId)
 
         vSend.setOnClickListener { sendMessage() }
+        vAddImage.setOnClickListener { openGallery() }
 
         initChat()
         getMessages()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) return
+
+        if (requestCode == GET_IMAGES_CODE) {
+            val clipData = data?.clipData
+            val singleData = data?.data
+            imageUriAdapter.imageUris.clear()
+            if (clipData != null) {
+                repeat(clipData.itemCount) { imageUriAdapter.imageUris += clipData.getItemAt(it).uri.toString() }
+            } else if (singleData != null) {
+                imageUriAdapter.imageUris += singleData.toString()
+            }
+        }
     }
 
     /** Initializes Chat View and [MessageAdapter]. */
@@ -52,6 +74,12 @@ class ChatActivity : AppCompatActivity() {
             isNestedScrollingEnabled = false
             layoutManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
             adapter = messageAdapter
+        }
+        with(vImages) {
+            setHasFixedSize(false)
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
+            adapter = imageUriAdapter
         }
     }
 
@@ -89,6 +117,16 @@ class ChatActivity : AppCompatActivity() {
 
         vInput.text.clear()
     }
+
+    /** Sends implicit intent to browse and select images and get the result in [onActivityResult]. */
+    private fun openGallery() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_GET_CONTENT
+            type = "image/*"
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        }
+        startActivityForResult(Intent.createChooser(intent, "Select image(s)"), GET_IMAGES_CODE)
+    }
 }
 
 private class MessageAdapter(val messages: MutableList<Message> = mutableListOf()) :
@@ -116,5 +154,23 @@ private class MessageAdapter(val messages: MutableList<Message> = mutableListOf(
 private class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val vText: TextView = itemView.vText
     val vSenderId: TextView = itemView.vSenderId
+}
+
+private class ImageUriAdapter(val imageUris: MutableList<String> = mutableListOf()) :
+    RecyclerView.Adapter<UriViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UriViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return UriViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UriViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int = imageUris.size
+}
+
+private class UriViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val vImage = itemView.vImage
 }
 
